d8da4b4b720b8a79ad3ce01ee9f6176fffa9f529
Chamath Goonetilleke <31180821+Chamath-Goonetilleke@users.noreply.github.com>
2025-01-19 00:54:58 +0530

    initial

---
+++ hopeOn/src/main/java/com/backend/hopeOn/HopeOnApplication.java
@@ -0,0 +1,13 @@
+package com.backend.hopeOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopeOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopeOnApplication.class, args);
+	}
+
+}
