188976533c61cba54ef66912176c6ce0ccb90779
Chamath Goonetilleke <31180821+Chamath-Goonetilleke@users.noreply.github.com>
2025-02-13 20:49:15 +0530

    hosting

--- hopeon_admin/src/service/httpService.js
+++ hopeon_admin/src/service/httpService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = "http://localhost:8080/api/v1"
+const baseURL = "http://13.61.16.165:8080/api/v1";
 axios.defaults.baseURL = baseURL;
 
 const methods ={
